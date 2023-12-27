b4fa8169fd25cc61c52aaa77f2059dde8fa67253
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2023-12-26 20:08:33 -0600

    Added logic for the start record button

--- RecordSystemAudio/Form1.cs
+++ RecordSystemAudio/Form1.cs
@@ -1,9 +1,15 @@
 using NAudio.CoreAudioApi;
+using NAudio.Wave;
+using System.Diagnostics;
 
 namespace RecordSystemAudio
 {
     public partial class Form1 : Form
     {
+        private string? _outputFilename;
+        private WasapiLoopbackCapture _wasapiLoopbackCapture;
+        private WaveFileWriter _waveFileWriter;
+
         public Form1()
         {
             InitializeComponent();
@@ -12,7 +18,40 @@ namespace RecordSystemAudio
 
         private void btnStart_Click(object sender, EventArgs e)
         {
+            var saveDialog = new SaveFileDialog();
+            saveDialog.Filter = "Wave files | *.wav";
 
+            if (saveDialog.ShowDialog() == DialogResult.OK)
+                return;
+
+            _outputFilename = saveDialog.FileName;
+            var selectedDevice = cbOutputDevice.SelectedItem as MMDevice;
+            _wasapiLoopbackCapture = new WasapiLoopbackCapture(selectedDevice);
+            _waveFileWriter = new WaveFileWriter(_outputFilename, _wasapiLoopbackCapture.WaveFormat);
+
+            _wasapiLoopbackCapture.DataAvailable += (s, e) =>
+            {
+                _waveFileWriter.Write(e.Buffer, 0, e.BytesRecorded);
+            };
+            _wasapiLoopbackCapture.RecordingStopped += (s, e) =>
+            {
+                _waveFileWriter.Dispose();
+                _wasapiLoopbackCapture?.Dispose();
+                btnStart.Enabled = true;
+                btnStop.Enabled = false;
+
+                var startInfo = new ProcessStartInfo
+                {
+                    FileName = Path.GetDirectoryName(_outputFilename),
+                    UseShellExecute = true
+                };
+
+                Process.Start(startInfo);
+            };
+
+            _wasapiLoopbackCapture.StartRecording();
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
         }
 
         private void btnStop_Click(object sender, EventArgs e)
